0bb33322744f8f86c812088d6ff8260d4d20921b
Ermyderis <71711154+Ermyderis@users.noreply.github.com>
2021-08-18 16:39:40 +0300

    Delete lucimqtt directory

--- lucimqtt/luasrc/controller/mosquittocontroler.lua
+++
@@ -1,7 +0,0 @@
-module("luci.controller.mosquittocontroler", package.seeall)
-
-function index()
-
-  entry( { "admin", "services", "mqttt"}, firstchild(), _("MQTT client"), 150)
-  entry( { "admin", "services", "mqttt", "Model" }, cbi("mqttmodel"), _("Model"), 1).leaf = true
-end
